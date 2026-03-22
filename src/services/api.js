37cc81dccfa849c63d433ffefded9505037b2891
lilia <liliahernandez2004@gmail.com>
2026-03-21 23:17:37 -0600

    entorno

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { saveRequest } from './offlineStorage';
 
 const api = axios.create({
-    baseURL: 'http://192.168.100.71:3000', // Updated for mobile access
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
     headers: {
         'Content-Type': 'application/json'
     }
